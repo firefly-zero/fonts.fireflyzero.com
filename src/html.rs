c7ca1eb751fe1a47ca720227f42a5bcedeb6a223
gram <git@orsinium.dev>
2024-04-25 10:42:10 +0200

    HTML generation

--- src/html.rs
+++ src/html.rs
@@ -1,1 +1,50 @@
+use crate::fonts::FONTS;
+use anyhow::{Context, Result};
+use minijinja::{context, Environment};
+use serde::Serialize;
+use std::fs;
+use std::path::Path;
 
+#[derive(Serialize)]
+struct Font {
+    family: &'static str,
+    width: u32,
+    height: u32,
+    encoding: &'static str,
+}
+
+pub(crate) fn build_html(root: &Path) -> Result<()> {
+    let mut env = Environment::new();
+    let templates = fs::read_dir("templates")?;
+    for file_path in templates {
+        let file_path = file_path?;
+        let content = fs::read_to_string(file_path.path())?;
+        let file_name = file_path.file_name();
+        let file_name = file_name.to_str().unwrap().to_string();
+        env.add_template_owned(file_name, content)?;
+    }
+
+    let tmpl = env.get_template("index.html.j2").context("get template")?;
+    let fonts = make_fonts();
+    let out_path = root.join("index.html");
+    let rendered = tmpl
+        .render(context!(fonts => fonts))
+        .context("render template")?;
+    fs::write(out_path, rendered).context("write html file")?;
+    Ok(())
+}
+
+fn make_fonts() -> Vec<Font> {
+    let mut result = Vec::new();
+    for (family, encoding, fonts) in FONTS {
+        for font in *fonts {
+            result.push(Font {
+                family,
+                encoding,
+                width: font.character_size.width,
+                height: font.character_size.height,
+            })
+        }
+    }
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ mod fonts;
 mod generator;
 mod html;
 use crate::generator::save_all_fonts;
+use crate::html::build_html;
 use std::path::PathBuf;
 
 fn main() {
@@ -10,5 +11,7 @@ fn main() {
     let fonts_path = root.join("fonts");
     std::fs::create_dir_all(&fonts_path).unwrap();
     let count = save_all_fonts(&fonts_path).unwrap();
-    println!("Generated {count} fonts")
+    println!("Generated {count} fonts");
+    build_html(&root).unwrap();
+    println!("Generated HTML pages");
 }
