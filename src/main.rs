4c2bd63cfabb8020c11c5b5a8c3394bdd9db3a6e
gram <git@orsinium.dev>
2024-04-25 09:40:44 +0200

    generate lock file

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::path::Path;
 
 fn main() {
     let args: Vec<_> = std::env::args().collect();
-    let default = "../fonts".to_string();
+    let default = "fonts".to_string();
     let root = args.get(1).unwrap_or(&default);
     let root = Path::new(root);
     let count = save_all_fonts(root).unwrap();
